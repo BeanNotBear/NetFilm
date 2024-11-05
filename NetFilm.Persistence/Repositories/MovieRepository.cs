d6ab3f61421c0ac52812a8fe9332c949108f34a4
NghiNV <nghinv03@gmail.com>
2024-11-05 13:51:36 +0700

    Movie repository

---
+++ NetFilm.Persistence/Repositories/MovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using NetFilm.Domain.Entities;
+using NetFilm.Domain.Interfaces;
+using NetFilm.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFilm.Persistence.Repositories
+{
+	public class MovieRepository : BaseRepository<Movie, Guid>, IMovieRepository
+	{
+		public MovieRepository(NetFilmDbContext context) : base(context)
+		{
+		}
+	}
+}
