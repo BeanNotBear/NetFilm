f2ede99286139beb5a7ff6e1c20e3992bb99dc1b
NghiNV <nghinv03@gmail.com>
2024-11-05 20:58:44 +0700

    modified access modifier of class country service

--- NetFilm.Infrastructure/Services/CountryService.cs
+++ NetFilm.Infrastructure/Services/CountryService.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetFilm.Infrastructure.Services
+﻿namespace NetFilm.Infrastructure.Services
 {
-	internal class CountryService
+	public class CountryService
 	{
 	}
 }
