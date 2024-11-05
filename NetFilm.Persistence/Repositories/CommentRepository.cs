ab87fb3dfbbb7cc214d347c62ea2202dca8ccf0e
NghiNV <nghinv03@gmail.com>
2024-11-05 13:54:38 +0700

    Comment repository

---
+++ NetFilm.Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using NetFilm.Domain.Entities;
+using NetFilm.Domain.Interfaces;
+using NetFilm.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFilm.Persistence.Repositories
+{
+	public class CommentRepository : BaseRepository<Comment, Guid>, ICommentRepository
+	{
+		public CommentRepository(NetFilmDbContext context) : base(context)
+		{
+		}
+	}
+}
