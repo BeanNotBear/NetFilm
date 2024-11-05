20cf7cdac95292c6d0e602f7a3d9bbb53d07b6c4
NghiNV <nghinv03@gmail.com>
2024-11-05 20:58:02 +0700

    Country Repository and Service base

---
+++ NetFilm.Domain/Interfaces/ICountryRepository.cs
@@ -0,0 +1,8 @@
+﻿using NetFilm.Domain.Entities;
+
+namespace NetFilm.Domain.Interfaces
+{
+	public interface ICountryRepository : IBaseRepository<Country, Guid>
+	{
+	}
+}
---
+++ NetFilm.Persistence/Repositories/CountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using NetFilm.Domain.Entities;
+using NetFilm.Domain.Interfaces;
+using NetFilm.Persistence.Data;
+
+namespace NetFilm.Persistence.Repositories
+{
+	public class CountryRepository : BaseRepository<Country, Guid>, ICountryRepository
+	{
+		public CountryRepository(NetFilmDbContext context) : base(context)
+		{
+		}
+	}
+}
